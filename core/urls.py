ffd3b8578cd9dbd5d034bacc9550615ff0e1e29d
mohammad <mohammadz270@yahoo.com>
2023-10-04 13:14:53 +0330

    final

--- core/urls.py
+++ core/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
